c5fa033ccb306ba70dcca7f53681433c9178c409
theowu23451 <theowu23451@gmail.com>
2019-07-29 14:08:06 +0800

    added truncate function

--- utils.py
+++ utils.py
@@ -41,3 +41,7 @@ def convert_to_dataset(data, batch_size):
     dataset = tf.data.Dataset.from_tensor_slices(data)
     dataset = dataset.batch(batch_size, drop_remainder=True)
     return dataset
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
